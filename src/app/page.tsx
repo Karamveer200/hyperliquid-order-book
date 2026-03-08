0f7e5a6f87b82afca18e52cdcb17bd750fbcc89a
Karamveer200 <stuntmania101@gmail.com>
2026-03-07 23:31:05 -0800

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col bg-black">
       <div className="flex min-h-0 flex-1 flex-col items-center md:px-4">
-        <div className="h-full w-full max-w-md py-2">
+        <div className="h-full w-full max-w-md pt-2 pb-5">
           <OrderBookWidget />
         </div>
       </div>
