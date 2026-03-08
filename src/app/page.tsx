951c8cda15d88b3562cc65d197ca81ca7f992c9f
Karamveer200 <stuntmania101@gmail.com>
2026-03-07 23:40:34 -0800

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { OrderBookWidget } from '@/components/OrderBook/OrderBookWidget';
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col bg-black">
-      <div className="flex min-h-0 flex-1 flex-col items-center md:px-4">
+      <div className="flex min-h-0 flex-1 flex-col items-center px-3">
         <div className="h-full w-full max-w-md pt-2 pb-5">
           <OrderBookWidget />
         </div>
